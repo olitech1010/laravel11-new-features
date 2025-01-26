d012d5209d1301b3889d6d6162a6c00d1178ce07
Clement Mensah <olitech1010@gmail.com>
2025-01-26 03:32:56 +0000

    layouts

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>
+        {{env('APP_NAME')}}
+    </title>
+    @vite(['rources/js/app.js', 'resources/css/app.css'])
+</head>
+<body class="bg-slate-100 text-slate-900">
+   <header>
+    <nav class="flex justify-between items-center py-4 px-8 bg-slate-900 text-white">
+        <a href="">Home</a>
+        <div class="flex items-center gap-4">
+            <a href="">login</a>
+            <a href="">register</a>
+        </div>
+    </nav>
+   </header>
+
+   <main  class="py-8 px-4 mx-auto max-w-screen-lg">
+    {{$slot}}
+   </main>
+</body>
+</html>
